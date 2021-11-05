e61c5d751d4167c628dadb05ebcb4ba25bb7e2d2
Mikhail <michaildrazdovich@gmail.com>
2021-11-05 12:04:50 +0300

    Improvements

--- src/api/app/index.ts
+++ src/api/app/index.ts
@@ -1,7 +1,12 @@
 // import { apiServer } from 'settings/web-services/api';
 import { IApiServerResponse } from 'interfaces/common/api';
+import { IDataObject } from 'interfaces';
 
-export function getTestMain(cfg: any): Promise<IApiServerResponse> {
+interface ITestMainData {
+    result?: IDataObject | IDataObject[] | null;
+}
+
+export function getTestMain(cfg: any): Promise<IApiServerResponse<ITestMainData>> {
     // return apiServer.get('/api/someUrl');
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -9,6 +14,11 @@ export function getTestMain(cfg: any): Promise<IApiServerResponse> {
                 data: {
                     result: null,
                 },
+                status: 200,
+                statusText: 'OK',
+                headers: {},
+                config: {},
+                request: null,
                 meta: cfg,
             });
         }, 500);
--- src/interfaces/common/api.ts
+++ src/interfaces/common/api.ts
@@ -1,16 +1,12 @@
 import { AxiosResponse } from 'axios';
 import { IDataObject } from 'interfaces/common';
 
-interface IData {
-    result?: IDataObject | IDataObject[] | null;
-}
-
-export interface IApiServerResponse {
-    data: AxiosResponse<IData | Array<any>>['data'];
+export interface IApiServerResponse<T> {
+    data: AxiosResponse<T>['data'];
     meta: IDataObject;
-    status?: AxiosResponse['status'];
-    statusText?: AxiosResponse['statusText'];
-    headers?: AxiosResponse['headers'];
-    config?: AxiosResponse['config'];
-    request?: AxiosResponse['request'];
+    status: AxiosResponse['status'];
+    statusText: AxiosResponse['statusText'];
+    headers: AxiosResponse['headers'];
+    config: AxiosResponse['config'];
+    request: AxiosResponse['request'];
 }
