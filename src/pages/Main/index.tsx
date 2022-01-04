0ca33a6857b0cbef02fa4b2f5b249123db1f4123
Mikhail <michaildrazdovich@gmail.com>
2022-01-04 10:30:14 +0300

    Husky test

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -18,6 +18,7 @@ const MainPage: FC<ComponentProps> = (props) => {
     const { app } = useSelector(getTestMainSelector);
 
     Logger.log(app);
+
     useEffect(() => {
         dispatch(getTestMainEffect());
     }, []); // eslint-disable-line
